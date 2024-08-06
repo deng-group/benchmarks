8d25bdcb266cdb4b964c6c2031c2903553864eef
Jerry Deng <zeyudeng@Jerrys-Mac-Studio.local>
2024-08-06 18:19:39 +0800

    update

---
+++ VASP/clean.sh
@@ -0,0 +1,1 @@
+rm */CONTCAR */DOSCAR */CHG* */EIG* */PRO* */REPO* */vasprun.xml* */XDATCAR */WAVECAR */OSZICAR */IBZKPT */vasp.out */PCDAT */vasprun.h5*
--- VASP/run.sh
+++ VASP/run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 module add vasp
 
-for i in `ls -d */`
+export machine_config="genoa_64cores"
+for i in `find . -mindepth 1 -maxdepth 1 -type d ! -name 'outcars' -exec basename {} \;`
 do
-	cd $i
-	echo $PWD
-	nkpt=`grep NKPTS outcar.* | awk '{print $4}'`
-	if [ $nkpt -gt 1 ]
-	then	
-		echo "$nkpt greater than 1, run vasp_std"
-		#mpirun -n 1 vasp_std > vasp.out
-	else
-		echo "$nkpt is equal to 1, run vasp_gam"
-		#mpirun -n 1 vasp_gam > vasp.out
-	fi
-	cd ..
+        cd $i
+        echo $PWD
+        mpirun -n 64 vasp_std > vasp.out
+        cp OUTCAR ../outcars/${i}.${machine_config}
+        cd ..
 done
